a14dc535f4d70fdd5b18ceec4bd313ef457692b4
phanha <phanthanhhak55hus@gmail.com>
2015-11-30 16:18:33 +0700

    fix

--- admin/config.php
+++ admin/config.php
@@ -1,14 +1,14 @@
 <?php
 // HTTP
-define('HTTP_SERVER',  $_SERVER['HTTP_HOST']);
+define('HTTP_SERVER',  'http://thungloa.byethost33.com/admin/');
 
 // HTTPS
-define('HTTPS_SERVER',  $_SERVER['HTTP_HOST']);
+define('HTTPS_SERVER',  'http://thungloa.byethost33.com');
 define('DIR_FOLDER', $_SERVER['DOCUMENT_ROOT']);
-define('HTTP_CATALOG',  $_SERVER['HTTP_HOST']);
+define('HTTP_CATALOG',  'http://thungloa.byethost33.com');
 // HTTPS
-define('HTTPS_SERVER',  $_SERVER['HTTP_HOST'].'/admin/');
-
+define('HTTPS_SERVER',  'http://thungloa.byethost33.com/admin/');
+define('HTTPS_CATALOG', 'http://thungloa.byethost33.com');
 // DIR
 define('DIR_APPLICATION', DIR_FOLDER.'/admin/');
 define('DIR_SYSTEM', DIR_FOLDER.'/system/');
@@ -25,9 +25,9 @@ define('DIR_CATALOG', DIR_FOLDER.'/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thungloa');
+define('DB_HOSTNAME', 'sql307.byethost33.com');
+define('DB_USERNAME', 'b33_16945949');
+define('DB_PASSWORD', 'phanthanhhak55hus');
+define('DB_DATABASE', 'b33_16945949_loathung');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // HTTP
-define('HTTP_SERVER',  $_SERVER['HTTP_HOST']);
+define('HTTP_SERVER',  'http://thungloa.byethost33.com/');
 
 // HTTPS
-define('HTTPS_SERVER',  $_SERVER['HTTP_HOST']);
+define('HTTPS_SERVER', 'http://thungloa.byethost33.com/');
 define('DIR_FOLDER', $_SERVER['DOCUMENT_ROOT']);
 // DIR
 define('DIR_APPLICATION', DIR_FOLDER.'/catalog/');
@@ -21,9 +21,9 @@ define('DIR_UPLOAD', DIR_FOLDER.'/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thungloa');
+define('DB_HOSTNAME', 'sql307.byethost33.com');
+define('DB_USERNAME', 'b33_16945949');
+define('DB_PASSWORD', 'phanthanhhak55hus');
+define('DB_DATABASE', 'b33_16945949_loathung');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
