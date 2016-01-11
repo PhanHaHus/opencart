ca57247fb6dda4091426623db3ddd94cc1ed82d9
phanha <phanthanhhak55hus@gmail.com>
2016-01-11 09:56:56 +0700

    fix

--- admin/config.php
+++ admin/config.php
@@ -28,6 +28,6 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'thungloa');
+define('DB_DATABASE', 'loathung');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
